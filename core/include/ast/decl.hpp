31fe2c9cbd04c55d6799370c213989c97ba0dd5d
Silicon27 <yangsilicon@gmail.com>
2026-03-22 01:40:47 +0100

    refactor: move TranslationUnitDecl to decl.hpp and enhance NamedDecl

--- core/include/ast/decl.hpp
+++ core/include/ast/decl.hpp
@@ -8,10 +8,26 @@
 #include "ast.hpp"
 
 namespace aion::ast {
-    class NamedDecl : public Decl {
-
-
+    /// The top-level declaration that represents the entire translation unit.
+    class TranslationUnitDecl : public Decl, public DeclContext {
+    public:
+        TranslationUnitDecl()
+            : Decl(Kind::translation_unit) {}
     };
+    static_assert(std::is_trivially_destructible_v<TranslationUnitDecl>);
+
+    class NamedDecl : public Decl {
+        IdentifierInfo* name;
+    public:
+        NamedDecl(Kind declaration_kind, IdentifierInfo* name)
+        : Decl(declaration_kind), name(name) {}
+
+        [[nodiscard]] IdentifierInfo* get_name() const { return name; }
+        [[nodiscard]] std::string_view get_identifier() const { return name->get_name(); }
+        void set_name(IdentifierInfo* new_name) { name = new_name; }
+    };
+
+
 }
 
 #endif //AION_DECL_HPP
